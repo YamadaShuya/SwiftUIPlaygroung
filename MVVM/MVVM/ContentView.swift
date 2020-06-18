b5fbf27bfd4f2e109ea242e30c1ae7f32e72e0b5
YamadaShuya <s21925026@nucba.ac.jp>
2020-06-18 14:44:15 +0900

    XCodeプロジェクト作成.

---
+++ MVVM/MVVM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MVVM
+//
+//  Created by 山田　修也 on 2020/06/18.
+//  Copyright © 2020 YamadaShuya. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
